1f5befa1d5475237aabdb72ce327dc72a05c5c10
Staethe <jzmynb@gmail.com>
2017-09-26 10:26:04 -0400

    Added rows of circles

--- OF_Project/src/ofApp.cpp
+++ OF_Project/src/ofApp.cpp
@@ -2,12 +2,32 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
+	//size of convas or window
+	// proportional size of square
+	//outline only, no fill
 
+	ofSetRectMode(OF_RECTMODE_CENTER);
+	ofSetBackgroundAuto(false);
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
+	ofBackground(ofColor::white);
+	ofSetColor(ofColor::green);
 
+
+	//Create four by four grid of circles
+	int grid_size = 4;
+	int radius = 50;
+	int left_margin = 100;
+	int top_margin = 100;
+	for (int i = 0; i < grid_size; i++){
+		for(int j = 0; j < grid_size; j++){
+			ofDrawCircle(left_margin + (i*ofGetWidth()/grid_size),
+						 top_margin + (j*ofGetHeight()/grid_size),
+						 radius);
+		}
+	}
 }
 
 //--------------------------------------------------------------
