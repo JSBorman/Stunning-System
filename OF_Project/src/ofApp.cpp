3345a391104a5079b451ae59707c77a8061db3f3
Staethe <jzmynb@gmail.com>
2017-10-02 19:01:35 -0400

    Removed extra default functions

--- OF_Project/src/ofApp.cpp
+++ OF_Project/src/ofApp.cpp
@@ -204,26 +204,6 @@ void ofApp::audioOut(float* buffer, int bufferSize, int nChannels){
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
 
 	//Check if the user clicked inside a circle
@@ -235,34 +215,4 @@ void ofApp::mousePressed(int x, int y, int button){
 			}
 		}
 	}
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
 }
--- OF_Project/src/ofApp.h
+++ OF_Project/src/ofApp.h
@@ -14,17 +14,7 @@ class ofApp : public ofBaseApp{
 		void updateBoard();			//Draw the board as line collides, etc.
 
 		void audioOut(float* buffer, int bufferSize, int nChannels);
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
 		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
 		
 		//Audio Vars
 		float frequencyFloat;
